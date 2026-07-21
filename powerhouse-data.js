8857dc4fb633e4e7f8087f00bfde8cfc2683dad8
woof-wolf <acrimonity@gmail.com>
2026-07-21 09:22:22 -0700

    PowerHouse v1.3.20b
    * Fixed One Hundred Hands tooltip not showing up.

--- powerhouse-data.js
+++ powerhouse-data.js
@@ -15441,7 +15441,7 @@ const oneHundredHands = {
   </div>'
 };
 
-dataPower[dataPower.length] = new Power(dataPower.length, 'One Hundred Hands', '<img src="img/power-icons/unarmed/Unarmed_OneHundredHands.png" />&nbsp;One Hundred Hands', 3, 14, pow++, 1, oneHundredHands.powers);
+dataPower[dataPower.length] = new Power(dataPower.length, 'One Hundred Hands', '<img src="img/power-icons/unarmed/Unarmed_OneHundredHands.png" />&nbsp;One Hundred Hands', 3, 14, pow++, 1, oneHundredHands.power);
 dataPower[dataPower.length-1].advantageList.push(new PowerAdvantage(0, null, null, null, null, null));
 dataPower[dataPower.length-1].advantageList.push(new PowerAdvantage(1, dataPowerAlias['R2'].name, dataPowerAlias['R2'].desc, 2, null, dataPowerAlias['R2'].tip));
 dataPower[dataPower.length-1].advantageList.push(new PowerAdvantage(2, dataPowerAlias['R3'].name, dataPowerAlias['R3'].desc, 2, 1, dataPowerAlias['R3'].tip));
